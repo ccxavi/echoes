4a262a0aafe80c9f1ab017b359b65e133f180344
ccxavi <ccxavillarin@gmail.com>
2025-12-21 18:37:24 +0800

    chore: remove leaderboard UI components

--- scripts/ui/mainMenu.gd
+++ scripts/ui/mainMenu.gd
@@ -1,27 +1,13 @@
 extends Control
 
-# --- REFERENCES ---
-# Ensure 'leaderboard' is the name of your instantiated scene in the editor
-@onready var leaderboard_ui = $Leaderboard 
-@onready var lb_button = $Menu/VBoxContainer/Leaderboard
-
 func _ready() -> void:
 	AudioManager.play_music("menu_theme", -10.0, true)
-	# 1. Hide leaderboard by default
-	if leaderboard_ui:
-		leaderboard_ui.visible = false
 
 # --- BUTTON HANDLERS ---
 
 func _on_play_pressed() -> void:
 	AudioManager.play_sfx("click")
 	get_tree().change_scene_to_file("res://scenes/ui/level_selection.tscn")
-
-func _on_leaderboard_pressed() -> void:
-	AudioManager.play_sfx("click")
-	if leaderboard_ui:
-		leaderboard_ui.visible = true # This makes the leaderboard appear again
-
 
 func _on_quit_pressed() -> void:
 	AudioManager.play_sfx("click")
